f451fee75db2fea004f907cec375c706c0223d5e
john.h3 <john_h3_2020@outlook.com>
2022-08-20 12:56:00 +0800

    update readme & commit solution

---
+++ src/daily/_20220820/Daily.kt
@@ -0,0 +1,32 @@
+package daily._20220820
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class Solution {
+    fun constructMaximumBinaryTree(nums: IntArray): TreeNode? {
+        if (nums.isEmpty()) return null
+        return recursiveConstruct(nums, 0, nums.size)
+    }
+
+    fun recursiveConstruct(nums: IntArray, start: Int, end: Int) : TreeNode {
+        var max = nums[start]
+        var maxIndex = start
+        for (i in start until end) {
+            if (nums[i] > max) {
+                max = nums[i]
+                maxIndex = i
+            }
+        }
+        val node = TreeNode(max)
+        if (maxIndex > start) {
+            node.left = recursiveConstruct(nums, start, maxIndex)
+        }
+        if (maxIndex < end - 1) {
+            node.right = recursiveConstruct(nums, maxIndex + 1, end)
+        }
+        return node
+    }
+}
