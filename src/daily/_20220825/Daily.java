f4e7d883cd458f72c3208c3ced3bed234df476ca
何亚晖 <heyahui@huayun.com>
2022-08-27 10:48:36 +0800

    commit solution

---
+++ src/daily/_20220825/Daily.java
@@ -0,0 +1,39 @@
+package daily._20220825;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author John.h3
+ * created on 2022/8/25
+ */
+public class Daily {
+    class Solution {
+        public List<Integer> findClosestElements(int[] arr, int k, int x) {
+            var r = Arrays.binarySearch(arr, x);
+            if (r < 0) {
+                r = -(r + 1);
+            }
+            if (r < 0) {
+                r++;
+            } else if (r == arr.length) {
+                r--;
+            }
+            var l = r - 1;
+            while (r - 1 - l != k) {
+                if (r < arr.length && l >= 0) {
+                    if (Math.abs(arr[r] - x) < Math.abs(arr[l] - x) || (arr[r] - x == x - arr[l] && arr[r] < arr[l])) {
+                        r++;
+                    } else {
+                        l--;
+                    }
+                } else if (l < 0) {
+                    r++;
+                } else {
+                    l--;
+                }
+            }
+            return Arrays.stream(arr, l + 1, r).boxed().toList();
+        }
+    }
+}
---
+++ src/daily/_20220826/Daily.kt
@@ -0,0 +1,30 @@
+package daily._20220826
+
+/**
+ * @author John.h3
+ * created on 2022/8/26
+ */
+
+class Solution {
+    fun maxProduct(nums: IntArray): Int {
+        var m1 = nums[0]
+        var m2: Int
+        if (nums[1] > m1) {
+            m1 = nums[1]
+            m2 = nums[0]
+        } else {
+            m2 = nums[1]
+        }
+        for (i in 2 until nums.size) {
+            if (nums[i] > m2) {
+                if (nums[i] > m1) {
+                    m2 = m1
+                    m1 = nums[i]
+                } else {
+                    m2 = nums[i]
+                }
+            }
+        }
+        return (m1 - 1) * (m2 - 1)
+    }
+}
